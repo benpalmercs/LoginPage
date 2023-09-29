9978b0334710e8bb5241638bbabdc6d9bd0288d0
benpalmercs <benpalmerthrowaway@outlook.com>
2023-09-29 10:31:18 -0500

    Initial Commit - Question2Screen

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-
+Builder.load_file("LoginPage.kv")
 class LoginPageApp(App):
-    pass
+    def build(self):
+        return LoginManager()
 
 class LoginManager(ScreenManager):
     pass
 
 class Question1Screen(Screen):
-    pass
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
+
+
+class Question2Screen(Screen):
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "error"
 
 class CorrectScreen(Screen):
-    pass
+    def advance(self):
+        self.manager.current = "question2"
 
 class ErrorScreen(Screen):
     pass
