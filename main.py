ff01293dd982172d9860dd6e79e4c9490a7dc6e8
benpalmercs <benpalmerthrowaway@outlook.com>
2023-10-03 10:32:35 -0500

    Initial Commit - LoginProject, started login project got to sign_in function

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
+# from kivy.app import App
+# from kivy.lang import Builder
+# from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Builder.load_file("LoginPage.kv")
+# class LoginPageApp(App):
+# def build(self):
+# return LoginManager()
+
+# class LoginManager(ScreenManager):
+# pass
+
+# class Question1Screen(Screen):
+# def answer_question(self,bool):
+# if bool:
+# self.manager.current = "correct"
+# else:
+# self.manager.current = "error"
+
+
+# class Question2Screen(Screen):
+# def answer_question(self,text):
+# if text.lower() == "deep in the heart of texas":
+# self.manager.current = "correct"
+# else:
+# self.ids.invalid_guess.text = "Invalid guess.\n Try again!"
+# self.ids.invalid_guess.color = "red"
+# class CorrectScreen(Screen):
+# def advance(self):
+# self.manager.current = "question2"
+
+# class ErrorScreen(Screen):
+# def advance(self):
+# self.manager.current = "question2"
+
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file("LoginPage.kv")
-class LoginPageApp(App):
+users = {"BP2007": "123!Codingrules", "B": "1"}
+
+
+class LoginProjectApp(App):
     def build(self):
         return LoginManager()
+
 
 class LoginManager(ScreenManager):
     pass
 
-class Question1Screen(Screen):
-    def answer_question(self,bool):
-        if bool:
-            self.manager.current = "correct"
+
+class SignInScreen(Screen):
+    def register(self):
+        self.manager.current = "register"
+    def sign_in(self,username,password):
+        if (username in users.items) and (users[username] == password):
+            self.manager.current = "welcome"
         else:
-            self.manager.current = "error"
+            self.ids.invalid_creds.text = "invalid credentials"
+            self.ids.invalid_creds.color = "red"
 
 
-class Question2Screen(Screen):
-    def answer_question(self,bool):
-        if bool:
-            self.manager.current = "correct"
-        else:
-            self.manager.current = "error"
 
-class CorrectScreen(Screen):
-    def advance(self):
-        self.manager.current = "question2"
-
-class ErrorScreen(Screen):
+class WelcomeScreen(Screen):
     pass
 
 
+class RegisterScreen(Screen):
+    pass
 
 
-LoginPageApp().run()
+Builder.load_file("LoginProject.kv")
+LoginProjectApp().run()
